d1d831364009a7fd86ca71e9768d0f68a8b843b7
Hutchinson-Christopher <Chhutchi@uvm.edu>
2014-04-29 19:16:27 -0400

    initial release

---
+++ War.java
@@ -0,0 +1,25 @@
+/** 
+* Chris Hutchinson
+* CS 110
+* Prof. Horton 
+* War - A class that represents the card game war where two players split a deck of cards.
+* Then with both players having a their stack of cards face down pull their top card and play
+* it face up. This is a battle and the player with the higher card takes both cards played and
+* adds them to the bottom of their deck. If the players battle and their cards are of equal 
+* value then there is a war, which each player takes the top card of their deck and places it
+* face down and then plays the next top card face up. Which ever player wins this battle takes
+* the 6 cards previously played and adds them to the bottom of their deck. If during a war the 
+* cards played face up are of equal value you follow the same protocal and war again until their
+* is a defined winner. If a player runs out of cards in his deck he immediatly loses.  
+*/
+
+public class War
+{
+   private Deck deck;        //the deck of cards for the game war
+   private int player;       //1 or 2 (2 is the computer) 
+   
+   
+   
+   
+}
+   
