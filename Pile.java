5f854165f101bd5c22fc96bb27479740563a5f7e
Hutchinson-Christopher <Chhutchi@uvm.edu>
2014-04-29 20:30:19 -0400

    rough draft

--- Pile.java
+++ Pile.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 * Pile - is an arraylist that represents the pile of cards that each player has during a game of war. 
 */
 
-public class Pile 
+public class Pile extends ArrayList 
 {
-   private ArrayList<Cards> pile;  //the pile of cards a player has 
+   private ArrayList<Card> pile;  //the pile of cards a player has 
    
    /**
    The default constructor for pile initializes an arraylist of cards.
@@ -25,11 +25,50 @@ public class Pile
    @param c is the card object that is added to the bottom of the pile.
    */
    
-   public add(Card c)
+   public void add(Card c)
    {
       pile.add(c);
       
    }
+   
+   /**
+   The topCard method takes the top card from the deck removes it and returns it to the method that called it. 
+   @return top is the top card of the pile. 
+   */
+   
+   public Card topCard()
+   {
+      Card top;      //variable for the top card of the pile  
+      
+      top = pile.remove(0);        // remove the top card of the pile and refrence it by the variabel top 
+      
+      return top;     // return top to the method that called it. 
+      
+   }
+   
+   /**
+   The size method uses a arraylist method to get the int value of the number of objects or card in the pile and
+   then returns the int value back to the method that called it. 
+   @return num is the int value of the number of cards in the pile. 
+   */
+   
+   public int size()
+   {
+      return pile.size();
+   }
+   
+   /**
+   The empty method uses an arraylist method to clear the entire arraylist so essential empties the pile. 
+   */
+   
+   public void empty()
+   {
+      pile.clear();
+   }
+   
+   
+    
+    
    
    
    
--- War.java
+++ War.java
@@ -15,8 +15,27 @@
 
 public class War
 {
+   final int SPLITDECK = 26; // a constant for the number of cards in a half a deck 
    private Deck deck;        //the deck of cards for the game war
-   private int player;       //1 or 2 (2 is the computer) 
+   private Deck player1;   // player ones deck 
+   private Deck player2;   // computer player's deck 
+   private int player1Cards; // number of cards player one has
+   private int player2Cards; // number of cards player two has 
+   
+   
+   
+   /**
+   The default constructor- which takes no arguments and returns nothing
+   */
+   
+   public War()
+   {
+      deck = new Deck();
+      
+      //split the deck for player one and two. 
+      for(int i = 0; i < SPLITDECK; i++)
+         
+    
    
    
    
